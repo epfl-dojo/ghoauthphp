8a75a8bc0939e13fc55fd81fa6e924a9b8d8c171
Nicolas Borboën <ponsfrilus@gmail.com>
2018-03-19 23:58:57 +0100

    tests

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Set default timezone
+date_default_timezone_set('Europe/Zurich');
+
+// App debug
+define('DEBUG', true);
+
+// oAuth
+define('OAUTH2_CLIENT_ID', 'c860801f2631edb52a63');
+define('OAUTH2_CLIENT_SECRET', 'f7b59da1114edfe22b1c496e5f9632edb17949e0');
+// gh
+define('GH_URL_AUTHORIZE','https://github.com/login/oauth/authorize');
+define('GH_URL_ACCESS_TOKEN','https://github.com/login/oauth/access_token');
+define('GH_URL_API','https://api.github.com/');
